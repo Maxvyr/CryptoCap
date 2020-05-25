d4e563fca6f4698a09045327014a6c1be771d7c9
Maxvyr <maxvy_@naver.com>
2020-05-25 14:54:45 +0200

    add widget Market Capitalisation + 24H volum

--- lib/view/homePage.dart
+++ lib/view/homePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../widget/cardCrypto.dart';
 import '../controller/variables.dart';
@@ -9,7 +10,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   void initState() {
     super.initState();
@@ -26,11 +26,63 @@ class _HomePageState extends State<HomePage> {
             width: widthTotal,
             height: MediaQuery.of(context).size.height,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+//              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(35.0),
+                  child: Container(
+                    color: white,
+                    child: Stack(
+                      alignment: Alignment.topCenter,
+                      children: <Widget>[
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.25,
+                        ),
+                        Positioned(
+                          top: MediaQuery.of(context).size.height * 0.015,
+                          child: Text("Market Capitalisation"),
+                        ),
+                        Positioned(
+                          top: MediaQuery.of(context).size.height * 0.05,
+                          child: Text(
+                            "247 910 Bn\$",
+                            style: TextStyle(fontSize: 35.0),
+                          ),
+                        ),
+                        Positioned(
+                          top: MediaQuery.of(context).size.height * 0.13,
+                          child: Container(
+                            color: blueMain,
+                            width: double.maxFinite,
+                            height: 3.0,
+                          ),
+                        ),
+                        Positioned(
+                          top: MediaQuery.of(context).size.height * 0.14,
+                          child: Text("24h Volume"),
+                        ),
+                        Positioned(
+                          top: MediaQuery.of(context).size.height * 0.18,
+                          child: Text(
+                            "126 50 Bn\$",
+                            style: TextStyle(fontSize: 35.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
                 SizedBox(height: 5.0),
-                cardCrypto("Bitcoin","https://static.wixstatic.com/media/34705a_11dec616ff0d41efa5215a10abf7d96f~mv2.png/v1/fit/w_190,h_56/34705a_11dec616ff0d41efa5215a10abf7d96f~mv2.png","9173.32","2.5"),
-                cardCrypto("Ethereum","https://static.wixstatic.com/media/34705a_ce63336c20794a45ba82aca6cc1cd9fc~mv2.png/v1/fit/w_190,h_56/34705a_ce63336c20794a45ba82aca6cc1cd9fc~mv2.png","233.38","4"),
+                cardCrypto(
+                    "Bitcoin",
+                    "https://static.wixstatic.com/media/34705a_11dec616ff0d41efa5215a10abf7d96f~mv2.png/v1/fit/w_190,h_56/34705a_11dec616ff0d41efa5215a10abf7d96f~mv2.png",
+                    "9173.32",
+                    "2.5"),
+                cardCrypto(
+                    "Ethereum",
+                    "https://static.wixstatic.com/media/34705a_ce63336c20794a45ba82aca6cc1cd9fc~mv2.png/v1/fit/w_190,h_56/34705a_ce63336c20794a45ba82aca6cc1cd9fc~mv2.png",
+                    "233.38",
+                    "4"),
               ],
             ),
           ),
