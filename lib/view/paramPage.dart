0774e4e5c318c06a02e74b381d518bfdc8fa5c2c
Maxvyr <maxvy_@naver.com>
2020-05-26 11:44:30 +0200

    add logo network page parameter

--- lib/view/paramPage.dart
+++ lib/view/paramPage.dart
@@ -8,24 +8,55 @@ class ParamPage extends StatefulWidget {
 }
 
 class _ParamPageState extends State<ParamPage> {
-
   @override
   void initState() {
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       appBar: appBarCustomSecond(context),
-      body : Container(
-        color: blueMain,
-        width: widthTotal,
-        height: double.infinity,
-        child: Center(
-          child: Text("Coming Soon"),
-        ),
+      body: Stack(
+        alignment: AlignmentDirectional.topStart,
+        children: <Widget>[
+          Container(
+            color: blueMain,
+            width: widthTotal,
+            height: double.infinity,
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.02,
+            left: MediaQuery.of(context).size.width * 0.25,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50.0),
+              child: Image.network(
+                "https://i.imgur.com/JZmCKtk.png",
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.02,
+            right: MediaQuery.of(context).size.width * 0.25,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50.0),
+              child: Image.network(
+                "https://i.imgur.com/misGMbi.png",
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.12,
+            left: MediaQuery.of(context).size.width*0.25,
+
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50.0),
+              child: Image.network(
+                "https://i.imgur.com/Z5twSh1.png",
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
