326babd70656d97148d0b5d7f44bd6cbc095bead
Maxvyr <maxvy_@naver.com>
2020-06-03 10:42:41 +0200

    v0 page each crypto

--- lib/view/cryptoPage.dart
+++ lib/view/cryptoPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../blocs/bloc.dart';
 
 class CryptoPage extends StatefulWidget {
   @override
@@ -6,10 +8,15 @@ class CryptoPage extends StatefulWidget {
 }
 
 class _CryptoPageState extends State<CryptoPage> {
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Coming Soon"),
+      child: BlocBuilder<CryptoBloc, CryptoState>(
+        builder: (context, state) {
+          return Text("Coming Soon");
+        },
+      ),
     );
   }
 }
--- lib/widget/coinModel.dart
+++ lib/widget/coinModel.dart
@@ -2,33 +2,44 @@ import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 class Coin extends Equatable {
-  final String name; //example BTC
-  final String fullName; // example Bitcoin
-  final double price; // example $10000
-  final String imageURL;  // logo BTC
+  final String name; // BTC
+  final String fullName; // Bitcoin
+  final double price; // $10000
+  final String imageURL; // logo BTC
   final double changePct24h; // +6.25%
+  final double changePct1h; // +6.25%
+  final double volume24h; // $11245592.32
+  final double marketCapCrypto; // $161299781696.08
+  final double availableSupply; // 18001162
 
   const Coin({
     @required this.name,
     @required this.fullName,
     @required this.price,
     @required this.imageURL,
-    @required this.changePct24h
+    @required this.changePct24h,
+    this.changePct1h,
+    this.volume24h,
+    this.marketCapCrypto,
+    this.availableSupply,
   });
 
   @override
-  List<Object> get props =>
-      [
+  List<Object> get props => [
         name,
         fullName,
         price,
         imageURL,
         changePct24h,
+        changePct1h,
+        volume24h,
+        marketCapCrypto,
+        availableSupply,
       ];
 
   @override
   String toString() =>
-      "Coin {name : $name, fullName : $fullName, price : $price, imageURL: $imageURL, changePct24h : $changePct24h}";
+      "Coin {name : $name, fullName : $fullName, price : $price, imageURL: $imageURL, changePct24h : $changePct24h, changePct1h : $changePct1h, volume24h : $volume24h, marketCapCrypto : $marketCapCrypto, availableSupply : $availableSupply}";
 
   factory Coin.fromJson(Map<String, dynamic> json) {
     return Coin(
@@ -37,6 +48,10 @@ class Coin extends Equatable {
       price: (json['RAW']['USD']['PRICE'] as num).toDouble(),
       imageURL: json['DISPLAY']['USD']['IMAGEURL'] as String,
       changePct24h: (json['RAW']['USD']['CHANGEPCT24HOUR'] as num).toDouble(),
+      changePct1h: (json['RAW']['USD']['CHANGEPCTHOUR'] as num).toDouble(),
+      volume24h: (json['RAW']['USD']['VOLUME24HOUR'] as num).toDouble(),
+      marketCapCrypto: (json['RAW']['USD']['MKTCAP'] as num).toDouble(),
+      availableSupply: (json['RAW']['USD']['SUPPLY'] as num).toDouble(),
     );
   }
 }
