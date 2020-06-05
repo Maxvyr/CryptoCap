66de608d8cc7989c2bbc3ef329559a2399cc23cd
Maxvyr <maxvy_@naver.com>
2020-06-05 09:42:00 +0200

    recover new value in the API

--- lib/widget/coinModel.dart
+++ lib/widget/coinModel.dart
@@ -11,6 +11,8 @@ class Coin extends Equatable {
   final double volume24h; // $11245592.32
   final double marketCapCrypto; // $161299781696.08
   final double availableSupply; // 18001162
+  final double open24HourValue; // 9786.31
+  final double open1HourValue; // 9981.56
 
   const Coin({
     @required this.name,
@@ -22,6 +24,8 @@ class Coin extends Equatable {
     this.volume24h,
     this.marketCapCrypto,
     this.availableSupply,
+    this.open24HourValue,
+    this.open1HourValue,
   });
 
   @override
@@ -35,11 +39,13 @@ class Coin extends Equatable {
         volume24h,
         marketCapCrypto,
         availableSupply,
+        open24HourValue,
+        open1HourValue,
       ];
 
   @override
   String toString() =>
-      "Coin {name : $name, fullName : $fullName, price : $price, imageURL: $imageURL, changePct24h : $changePct24h, changePct1h : $changePct1h, volume24h : $volume24h, marketCapCrypto : $marketCapCrypto, availableSupply : $availableSupply}";
+      "Coin {name : $name, fullName : $fullName, price : $price, imageURL: $imageURL, changePct24h : $changePct24h, changePct1h : $changePct1h, volume24h : $volume24h, marketCapCrypto : $marketCapCrypto, availableSupply : $availableSupply, open24HourValue : $open24HourValue, open1HourValue : $open1HourValue}";
 
   factory Coin.fromJson(Map<String, dynamic> json) {
     return Coin(
@@ -52,6 +58,8 @@ class Coin extends Equatable {
       volume24h: (json['RAW']['USD']['VOLUME24HOUR'] as num).toDouble(),
       marketCapCrypto: (json['RAW']['USD']['MKTCAP'] as num).toDouble(),
       availableSupply: (json['RAW']['USD']['SUPPLY'] as num).toDouble(),
+      open24HourValue: (json['RAW']['USD']['OPEN24HOUR'] as num).toDouble(),
+      open1HourValue: (json['RAW']['USD']['OPENHOUR'] as num).toDouble(),
     );
   }
 }
