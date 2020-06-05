65f2534e03c1ec4565031096798a033331aced01
Maxvyr <maxvy_@naver.com>
2020-06-05 11:29:02 +0200

    update value coins list in  crypto page

--- lib/view/cryptoPage.dart
+++ lib/view/cryptoPage.dart
@@ -1,14 +1,15 @@
+import 'package:cryptocap/blocs/bloc.dart';
+import 'package:cryptocap/widget/errorCryptoWidget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sparkline/flutter_sparkline.dart';
 import 'package:url_launcher/url_launcher.dart';
-import '../repositories/cryptoRepository.dart';
 import '../controller/routes.dart';
 import '../controller/variables.dart';
 import '../widget/containerMain.dart';
 import '../widget/cryptoLoadingWidget.dart';
 import '../widget/appBarCustom.dart';
-import '../widget/coinModel.dart';
 
 class CryptoPage extends StatefulWidget {
   @override
@@ -16,8 +17,6 @@ class CryptoPage extends StatefulWidget {
 }
 
 class _CryptoPageState extends State<CryptoPage> {
-  final _cryptoRepository = CryptoRepository();
-  int _page = 0;
   int _index = 0;
   String _cryptoName;
   String _urlBuyCrypto =
@@ -46,173 +45,173 @@ class _CryptoPageState extends State<CryptoPage> {
       body: SafeArea(
         child: ContainerMain(
           Center(
-            child: FutureBuilder(
-              future: _cryptoRepository.getTopCoins(page: _page),
-              builder: (context, snapshot) {
-                // si pas de data alors affichage cercle de recherche
-                if (!snapshot.hasData) {
-                  return cryptoLoadingWidget(context);
-                }
-                /*
-                  stock les coin dans un list et
-                  l'index de cette liste est l'index qui correspond au click de l'utilisateur
-                  ceci permet d'afficher des détails de la crypto
-                */
-                // init variable
-                final List<Coin> coins = snapshot.data;
-                final coin = coins[_index];
-
-                /* calcPrevious value
-                 * d'abord transfo le pourcentage en valeur comppris entre  -1 et 1
-                 * ajouter 1 a la valeur abs du résultat
-                 * si résultat positif * par addition sinon /
-                */
-                var _changeTempo24h = coin.changePct24h / 100;
-                var _changeTempo1h = coin.changePct1h / 100;
-                price1d = (_changeTempo24h <= 0)
-                    ? coin.price * (1 + _changeTempo24h.abs())
-                    : coin.price / (1 + _changeTempo24h.abs());
-                price1h = (_changeTempo1h <= 0)
-                    ? coin.price * (1 + _changeTempo1h.abs())
-                    : coin.price / (1 + _changeTempo1h.abs());
-
-                _cryptoValue = [
-                  price1d,
-                  price1h,
-                  coin.price,
-                ];
-                return SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      SizedBox(height: screen.size.height * 0.001),
-
-                      // graph
-                      Stack(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(35.0),
-                            child: Container(
-                              width: screen.size.width * 0.9,
-                              height: screen.size.height * 0.5,
-                              color: white,
-                            ),
-                          ),
-                          Positioned(
-                            top: screen.size.height * 0.05,
-                            left: 5.0,
-                            child:
-                                Text("${_cryptoValue[2].toStringAsFixed(2)}"),
-                          ),
-                          Positioned(
-                            top: screen.size.height * 0.25,
-                            left: 5.0,
-                            child:
-                                Text("${_cryptoValue[1].toStringAsFixed(2)}"),
-                          ),
-                          Positioned(
-                            top: screen.size.height * 0.4,
-                            left: 5.0,
-                            child:
-                                Text("${_cryptoValue[0].toStringAsFixed(2)}"),
-                          ),
-                          Positioned(
-                            top: screen.size.height * 0.05,
-                            left: screen.size.width * 0.15,
-                            child: Container(
-                              height: screen.size.height * 0.4,
-                              child: Sparkline(
-                                data: _cryptoValue,
-                                pointsMode: PointsMode.all,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-
-                      // Button Buy crypto - Coinbase
-                      SizedBox(height: 25.0),
-                      RaisedButton(
-                        onPressed: () {
-                          launch(_urlBuyCrypto);
-                        },
-                        elevation: 8.0,
-                        color: Colors.blueGrey,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        child: Container(
-                          width: screen.size.width * 0.8,
-                          child: Center(
-                            child: Text(
-                              "Buy Crypto",
-                              style: TextStyle(
-                                fontFamily: fontApp,
-                                fontSize: 22.0,
-                                fontWeight: FontWeight.w800,
-                                shadows: [
-                                  Shadow(
-                                    color: grey,
-                                    offset: Offset(2.0, 2.0),
-                                    blurRadius: 4.0,
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 25.0),
-
-                      //bloc data crypto
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(35.0),
-                        child: Container(
-                          color: white,
-                          width: screen.size.width * 0.9,
-                          child: Column(
-                            children: [
-                              SizedBox(height: 5.0),
-                              rowCrypto(
-                                textDescribe: "Market Cap",
-                                value: "\$ ${coin.marketCapCrypto}",
-                              ),
-                              rowCrypto(
-                                textDescribe: "24h Volume",
-                                value:
-                                    "\$ ${coin.volume24h.toStringAsFixed(2)}",
-                              ),
-                              rowCrypto(
-                                textDescribe: "Available Supply",
-                                value:
-                                    "${coin.availableSupply.toStringAsFixed(2)} ${coin.name}",
-                              ),
-                              rowCrypto(
-                                textDescribe: "% change 1h",
-                                value:
-                                    "${coin.changePct1h.toStringAsFixed(2)} %",
-                                colorValue: coin.changePct1h >= 0 ? green : red,
-                              ),
-                              rowCrypto(
-                                textDescribe: "% change 1d",
-                                value:
-                                    "${coin.changePct24h.toStringAsFixed(2)} %",
-                                colorValue:
-                                    coin.changePct24h >= 0 ? green : red,
-                              ),
-                              SizedBox(height: 5.0),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
+            child: BlocBuilder<CryptoBloc, CryptoState>(
+              builder: (context, state) {
+                return _buildPage(state, screen);
               },
             ),
           ),
         ),
       ),
     );
+  }
+
+  _buildPage(CryptoState state, MediaQueryData screen) {
+    // si pas de data alors affichage cercle de recherche
+    if (state is CryptoLoading) {
+      return cryptoLoadingWidget(context);
+    } else if (state is CryptoLoaded) {
+      /*
+                  stock les coin dans un list et
+                  l'index de cette liste est l'index qui correspond au click de l'utilisateur
+                  ceci permet d'afficher des détails de la crypto
+                */
+      // init variable
+      final coin = state.coins[_index];
+
+      /* calcPrevious value
+                 * d'abord transfo le pourcentage en valeur comppris entre  -1 et 1
+                 * ajouter 1 a la valeur abs du résultat
+                 * si résultat positif * par addition sinon /
+                */
+      var _changeTempo24h = coin.changePct24h / 100;
+      var _changeTempo1h = coin.changePct1h / 100;
+      price1d = (_changeTempo24h <= 0)
+          ? coin.price * (1 + _changeTempo24h.abs())
+          : coin.price / (1 + _changeTempo24h.abs());
+      price1h = (_changeTempo1h <= 0)
+          ? coin.price * (1 + _changeTempo1h.abs())
+          : coin.price / (1 + _changeTempo1h.abs());
+
+      _cryptoValue = [
+        price1d,
+        price1h,
+        coin.price,
+      ];
+      return Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: screen.size.height * 0.001),
+
+              // graph
+              Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(35.0),
+                    child: Container(
+                      width: screen.size.width * 0.9,
+                      height: screen.size.height * 0.5,
+                      color: white,
+                    ),
+                  ),
+                  Positioned(
+                    top: screen.size.height * 0.05,
+                    left: 5.0,
+                    child: Text("${_cryptoValue[2].toStringAsFixed(2)}"),
+                  ),
+                  Positioned(
+                    top: screen.size.height * 0.25,
+                    left: 5.0,
+                    child: Text("${_cryptoValue[1].toStringAsFixed(2)}"),
+                  ),
+                  Positioned(
+                    top: screen.size.height * 0.4,
+                    left: 5.0,
+                    child: Text("${_cryptoValue[0].toStringAsFixed(2)}"),
+                  ),
+                  Positioned(
+                    top: screen.size.height * 0.05,
+                    left: screen.size.width * 0.15,
+                    child: Container(
+                      height: screen.size.height * 0.4,
+                      child: Sparkline(
+                        data: _cryptoValue,
+                        pointsMode: PointsMode.all,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              // Button Buy crypto - Coinbase
+              SizedBox(height: 25.0),
+              RaisedButton(
+                onPressed: () {
+                  launch(_urlBuyCrypto);
+                },
+                elevation: 8.0,
+                color: Colors.blueGrey,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Container(
+                  width: screen.size.width * 0.8,
+                  child: Center(
+                    child: Text(
+                      "Buy Crypto",
+                      style: TextStyle(
+                        fontFamily: fontApp,
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w800,
+                        shadows: [
+                          Shadow(
+                            color: grey,
+                            offset: Offset(2.0, 2.0),
+                            blurRadius: 4.0,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25.0),
+
+              //bloc data crypto
+              ClipRRect(
+                borderRadius: BorderRadius.circular(35.0),
+                child: Container(
+                  color: white,
+                  width: screen.size.width * 0.9,
+                  child: Column(
+                    children: [
+                      SizedBox(height: 5.0),
+                      rowCrypto(
+                        textDescribe: "Market Cap",
+                        value: "\$ ${coin.marketCapCrypto}",
+                      ),
+                      rowCrypto(
+                        textDescribe: "24h Volume",
+                        value: "\$ ${coin.volume24h.toStringAsFixed(2)}",
+                      ),
+                      rowCrypto(
+                        textDescribe: "Available Supply",
+                        value:
+                            "${coin.availableSupply.toStringAsFixed(2)} ${coin.name}",
+                      ),
+                      rowCrypto(
+                        textDescribe: "% change 1h",
+                        value: "${coin.changePct1h.toStringAsFixed(2)} %",
+                        colorValue: coin.changePct1h >= 0 ? green : red,
+                      ),
+                      rowCrypto(
+                        textDescribe: "% change 1d",
+                        value: "${coin.changePct24h.toStringAsFixed(2)} %",
+                        colorValue: coin.changePct24h >= 0 ? green : red,
+                      ),
+                      SizedBox(height: 5.0),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else if (state is CryptoError) {
+      return errorCrytoWidget(context);
+    }
   }
 
   Widget rowCrypto({
